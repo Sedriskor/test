67a02669877d2ee40d19c800ecf03d8b1287b5e5
Sedriskor <78597096+Sedriskor@users.noreply.github.com>
2021-03-04 11:42:00 +0800

    ver.2 add ValueError Detect

--- collatz.py
+++ collatz.py
@@ -1,16 +1,32 @@
-def collatz(number):
+def collatz(number): #collatz sequence funtion
     if number % 2 == 0:
-        number = number//2
+        number = number // 2
+        print(number)
+
     else:
-        number = 3*number + 1
+        number = 3 * number + 1
+        print(number)
+
     return number
 
 
-number = int(input('Plas enter a number: '))
+def user_enter():
+    while True:
+        try:  
+            number = int(input('Pls enter a number: '))
 
-while True:
-    number = collatz(number)
-    print(number)
-    if number ==1:
+        except ValueError:
+            print('Pls enter a number not a str !!')
+            continue
+
+        return number
+
+
+number = user_enter()
+while True:  #main program loop
+    number = collatz(number)  
+    if number == 1:
         break
+
+
 
